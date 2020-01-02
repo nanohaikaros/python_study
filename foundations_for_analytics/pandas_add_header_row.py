87dcd2fe0fce51921d78b6d8e463bcc2a3819cf8
ikarosnanoha <1371689491@qq.com>
2020-01-02 22:09:53 +0800

    1

---
+++ foundations_for_analytics/pandas_add_header_row.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import sys
+input_file = sys.argv[1]
+output_file = sys.argv[2]
+header_list = ['Supplier Name', 'Invoice Number', 'part Number', 'Cost', 'Purchase Date']
+data_frame = pd.read_csv(input_file, header=None, names=header_list)
+data_frame.to_csv(output_file, index=False)
